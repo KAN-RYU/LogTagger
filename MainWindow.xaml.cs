f5023f5fb0f63e529c57506046063b48dab7d02c
PureBlueWater <chsrlyhsn@gmail.com>
2024-12-28 22:25:53 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogTagger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Log_Open_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                DefaultExt = ".txt",
+                Title = "채팅 파일 열기",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            };
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result != true) return;
+
+            string filename = dlg.FileName;
+            Contents_Box.Text = filename;
+        }
+    }
+}
